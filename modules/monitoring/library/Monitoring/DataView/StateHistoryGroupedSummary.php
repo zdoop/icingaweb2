8b996ebcc2985fbd2b62d3fc892d11dfe272267c
Alexander Klimov <alexander.klimov@netways.de>
2014-10-28 17:37:51 +0100

    Add DataView for StateHistoryGroupedSummary
    refs #4474

---
+++ modules/monitoring/library/Monitoring/DataView/StateHistoryGroupedSummary.php
@@ -0,0 +1,41 @@
+<?php
+// {{{ICINGA_LICENSE_HEADER}}}
+// {{{ICINGA_LICENSE_HEADER}}}
+
+namespace Icinga\Module\Monitoring\DataView;
+
+class StateHistoryGroupedSummary extends DataView
+{
+    /**
+     * Retrieve columns provided by this view
+     *
+     * @return array
+     */
+    public function getColumns()
+    {
+        return array(
+            'cnt_host_up',
+            'cnt_host_down',
+            'cnt_host_unreachable',
+            'cnt_service_ok',
+            'cnt_service_warning',
+            'cnt_service_critical',
+            'cnt_service_unknown',
+
+            'hour',
+            '6h',
+            'day',
+            '3d',
+            'week',
+            'month',
+            'year',
+
+            'timestamp'
+        );
+    }
+
+    public function getSortRules()
+    {
+        return array();
+    }
+}
