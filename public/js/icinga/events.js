3f32f844c03390f3fc9f00b42babc1d38b793b06
Matthias Jentsch <matthias.jentsch@netways.de>
2015-03-26 12:56:49 +0100

    Remove added event handlers again to prevent leakage
    refs #7932

--- public/js/icinga/events.js
+++ public/js/icinga/events.js
@@ -616,6 +616,10 @@
             $(document).off('change', 'form input.autosubmit', this.submitForm);
             $(document).off('focus', 'form select[data-related-radiobtn]', this.autoCheckRadioButton);
             $(document).off('focus', 'form input[data-related-radiobtn]', this.autoCheckRadioButton);
+
+            $(document).off('keydown', 'table.multiselect tr[href]', this.rowKeypress);
+            $(document).off('focusin', 'table.multiselect tr[href]', this.rowFocus);
+            $(document).off('focusout', 'table.multiselect tr[href]', this.rowFocusOut);
         },
 
         destroy: function() {
